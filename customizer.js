52cd201dc0f0654128250fc65cd2c6bf227b2f99
dumbledores <58190783+dumbledores@users.noreply.github.com>
2019-11-26 14:44:37 -0600

    Update customizer.js

--- customizer.js
+++ customizer.js
@@ -1,3 +1,18 @@
 <script>
+  
+  // Initialize order fields 
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+
+// Add new text field to order comments section at checkout
+ec.order.extraFields.how_you_found_us = {
+    'title': 'How did you find us?',
+    'textPlaceholder': 'Describe here please!',
+    'type': 'text',
+    'required': false,
+    'checkoutDisplaySection': 'payment_details', // show new field in order comments block
+    'orderDetailsDisplaySection': 'order_comments' // show saved data in order comments block in order details to merchant and customer
+};
+  
   console.log("custom java script");
 </script>
