c0af63877539515b4f44ef0baebda880e9a58946
dumbledores <58190783+dumbledores@users.noreply.github.com>
2019-11-26 14:48:23 -0600

    Update customizer.js

--- customizer.js
+++ customizer.js
@@ -1,18 +1,39 @@
-<script>
-  
-  // Initialize order fields 
-ec.order = ec.order || {};
-ec.order.extraFields = ec.order.extraFields || {};
+/*
+ * Show a promo 'Free shipping' message on the cart page. 
+ * In the example below, users will see a message on the cart page 
+ * informing them that they will qualify for free shipping if their 
+ * order is more than $99
+ */
 
-// Add new text field to order comments section at checkout
-ec.order.extraFields.how_you_found_us = {
-    'title': 'How did you find us?',
-    'textPlaceholder': 'Describe here please!',
-    'type': 'text',
-    'required': false,
-    'checkoutDisplaySection': 'payment_details', // show new field in order comments block
-    'orderDetailsDisplaySection': 'order_comments' // show saved data in order comments block in order details to merchant and customer
-};
-  
-  console.log("custom java script");
-</script>
+var promoMessage = "Orders $99 and up ship free!";
+var minSubtotal = 99;
+var widgets;
+
+// Calculating subtotal and displaying the message
+var checkSubtotal = function(order) {
+  if (order) {
+    var subtotal = order.total - order.shipping;
+    if (subtotal > minSubtotal) {
+      alert(promoMessage);
+    }  
+  }
+}
+
+// Detecting whether we're on the cart page and get the cart info
+Ecwid.OnPageLoaded.add(function(page) {
+  widgets = Ecwid.getInitializedWidgets();
+
+  // if storefront widget is present on this page
+  for (i = 0; i < widgets.length ; i++) {
+    if (widgets[i] == "ProductBrowser") {
+      if ('CART' == page.type) {
+        Ecwid.Cart.calculateTotal(function(order) {
+          checkSubtotal(order);
+        });
+      }
+    }
+  }
+});
+
+// Get color value for the message and store it in color variable
+var color = Ecwid.getAppPublicConfig(appId);
